eae46c7d581c87e7a7d4ae226092230329edda39
Andriyann <50099343+Andriyann@users.noreply.github.com>
2020-09-19 09:10:03 -0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "avrill.v", quotes: "Hai sayang" },
+    { author: "antjedtta_", quotes: "Pi tau ga bedanya pelangi ama kamu? Kalo pelangi di awan kamu dihatiku" },
+    { author: "standinshd", quotes: "Gaada yang bisa misahin kita kecuali ajal" },
     { author: "standinshd", quotes: "I love you." },
     { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "standinshd", quotes: "Mungkin di ata orang lain kamu biasa tapii dihatiku kamu segalanyaaa" },
+    { author: "standinshd", quotes: "I'm sorry for loving you pi:**." },
 ];
