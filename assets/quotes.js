0576532780a2d5aaf2e483d4db13a26157fb7eee
Andriyann <50099343+Andriyann@users.noreply.github.com>
2020-09-19 09:13:29 -0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Hai sayang" },
-    { author: "antjedtta_", quotes: "Pi tau ga bedanya pelangi ama kamu? Kalo pelangi di awan kamu dihatiku" },
-    { author: "standinshd", quotes: "Gaada yang bisa misahin kita kecuali ajal" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "Mungkin di ata orang lain kamu biasa tapii dihatiku kamu segalanyaaa" },
-    { author: "standinshd", quotes: "I'm sorry for loving you pi:**." },
+    { author: "Andrian", quotes: "Hai sayang" },
+    { author: "Ian nya ipi", quotes: "Pi tau ga bedanya pelangi ama kamu? Kalo pelangi di awan kamu dihatiku" },
+    { author: "Ian", quotes: "Gaada yang bisa misahin kita kecuali ajal" },
+    { author: "Ian", quotes: "I love you." },
+    { author: "Ian", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "Ian", quotes: "Mungkin di mata orang lain kamu biasa tapii dihatiku kamu segalanyaaa" },
+    { author: "Ian", quotes: "I'm sorry for loving you pi:**." },
 ];
